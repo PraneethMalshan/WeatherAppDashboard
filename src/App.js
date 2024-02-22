29577002bdd6f24aa41f96032c2ac3e7fb275742
PraneethMalshan <praneethmalshan12345@gmail.com>
2024-02-23 00:16:44 +0530

    Added TemperatureAndDetails

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import TopButtons from './components/TopButtons';
 import Puts from './components/Puts';
 import TimeAndLocation from './components/TimeAndLocation';
+import TemperatureAndDetails from './components/TemperatureAndDetails';
 function App() {
   return (
-    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-b from-cyan-500 to-blue-900 h-fit shadow-xl shadow-gray-400 ">
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-green-500  to-blue-900 h-fit shadow-xl shadow-gray-400 ">
       <TopButtons />
       <Puts />
       <TimeAndLocation />
+      <TemperatureAndDetails />
     </div>
   );
 }
