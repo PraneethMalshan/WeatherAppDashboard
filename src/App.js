928eb6c29f16a912e34cb67cc5c68b6c06f70a4f
PraneethMalshan <praneethmalshan12345@gmail.com>
2024-02-22 21:47:48 +0530

    Added TimeAndLocation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import TopButtons from './components/TopButtons';
 import Puts from './components/Puts';
+import TimeAndLocation from './components/TimeAndLocation';
 function App() {
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-b from-cyan-500 to-blue-900 h-fit shadow-xl shadow-gray-400 ">
       <TopButtons />
       <Puts />
+      <TimeAndLocation />
     </div>
   );
 }
---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function TimeAndLocation() {
+  return (
+    <div>
+        <div className='flex items-center justify-center my-6'>
+            <p className='text-white text-xl font-extralight'>
+                Thursday, 22 February 2024 | Local time: 12:30 PM
+            </p>
+        </div>
+
+        <div className='flex items-center justify-center my-3'>
+            <p className='text-white text-3xl font-medium'>
+                Galle, Elpitiya
+            </p>
+        </div>
+    </div>
+    
+  )
+}
+
+export default TimeAndLocation
