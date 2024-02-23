5d12844aad3ac7ce54e9bb9459abb007f9807737
PraneethMalshan <praneethmalshan12345@gmail.com>
2024-02-23 08:43:13 +0530

    Added API part Of local date time

--- src/App.js
+++ src/App.js
@@ -4,13 +4,26 @@ import TopButtons from './components/TopButtons';
 import Puts from './components/Puts';
 import TimeAndLocation from './components/TimeAndLocation';
 import TemperatureAndDetails from './components/TemperatureAndDetails';
+import Forecast from './components/Forecast';
+import getFormattedWeatherData from './services/weatherService';
+
+
 function App() {
+  const fetchweather = async () => {
+    const data = await getFormattedWeatherData({ q: "london" });
+    console.log(data);
+  };
+
+  fetchweather();
+
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-green-500  to-blue-900 h-fit shadow-xl shadow-gray-400 ">
       <TopButtons />
       <Puts />
       <TimeAndLocation />
       <TemperatureAndDetails />
+      <Forecast title="hourly forecast"/>
+      <Forecast title="daily forecast"/>
     </div>
   );
 }
